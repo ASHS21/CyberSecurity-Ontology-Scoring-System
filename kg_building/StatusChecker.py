91af1f150c23152e1da9a11ef282a791f8b1704a
Ali Almoharif <51306868+ASHS21@users.noreply.github.com>
2024-02-27 01:52:31 +0000

    Finished the KG, data simulation needed now

---
+++ kg_building/StatusChecker.py
@@ -0,0 +1,60 @@
+import json
+from neo4j import GraphDatabase
+import pandas as pd
+
+class Neo4jConnection:
+    def __init__(self, config_path):
+        self.config = self.load_config(config_path)
+        self.driver = GraphDatabase.driver(self.config['neo4j']['uri'], 
+                                           auth=(self.config['neo4j']['user'], 
+                                                 self.config['neo4j']['password']))
+    
+    @staticmethod
+    def load_config(config_path):
+        with open(config_path, 'r') as file:
+            return json.load(file)
+    
+    def close(self):
+        if self.driver:
+            self.driver.close()
+
+    def update_policy_weights(self, policy_types):
+        with self.driver.session() as session:
+            for policy in policy_types:
+                session.run(
+                    f"""
+                    MATCH (p:{policy})-[r:CONTRIBUTES_WEIGHT_TO]->()
+                    WHERE p.status IN ['bypassed', 'ignored', 'violated']
+                    SET r.weight = 0
+                    """
+                )
+
+
+def main():
+
+    config_path = 'config.json'  
+    
+    # Initialize connection
+    conn = Neo4jConnection(config_path)
+    
+    policy_types = [
+        'Governance_Policies',
+        'Assurance_Policies',
+        'Asset_Management_Policies',
+        'Emerging_Technologies_Policies',
+        'Security_Operations_Policies',
+        'Change_Management_Policies'
+    ]
+
+    
+    
+    # Update policy weights
+    conn.update_policy_weights(policy_types)
+    
+    print("Policy weights have been updated.")
+    
+    # Close the Neo4j connection
+    conn.close()
+
+if __name__ == "__main__":
+    main()
